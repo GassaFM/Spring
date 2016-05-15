2873ab68ce6656e26526f65ff0eca99abd6e6b7a
Ivan Kazmenko <gassa@mail.ru>
2016-05-15 16:27:55 +0000

    tins2016@2195

--- source/io.d
+++ source/io.d
@@ -7,15 +7,15 @@ class Io
 {
 	Io parent;
 	Io [] child;
-	int x;
-	int y;
+	int relX;
+	int relY;
 	int w;
 	int h;
 
 	final void draw (ALLEGRO_BITMAP * buffer)
 	{
 		ALLEGRO_BITMAP * curBuffer =
-		    al_create_sub_bitmap (buffer, x, y, w, h);
+		    al_create_sub_bitmap (buffer, relX, relY, w, h);
 		drawThisPre (curBuffer);
 		foreach (c; child)
 		{
@@ -31,5 +31,43 @@ class Io
 
 	void drawThisPost (ALLEGRO_BITMAP * buffer)
 	{
+	}
+
+	final bool coordPostOrder (alias fun) (int posX, int posY)
+	{
+		posX -= relX;
+		posY -= relY;
+		if (posX < 0 || w <= posX || posY < 0 || h <= posY)
+		{
+			return false;
+		}
+		foreach (c; child)
+		{
+			if (c.coordPostOrder !(fun) (posX, posY))
+			{
+				return true;
+			}
+		}
+		return fun (posX, posY);
+	}
+
+	final bool click (int posX, int posY)
+	{
+		return coordPostOrder !(clickThis) (posX, posY);
+	}
+
+	bool clickThis (int posX, int posY)
+	{
+		return false;
+	}
+
+	final bool unclick (int posX, int posY)
+	{
+		return coordPostOrder !(unclickThis) (posX, posY);
+	}
+
+	bool unclickThis (int posX, int posY)
+	{
+		return false;
 	}
 }
